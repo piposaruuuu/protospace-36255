be4e69ce04b20f24dbcd6b3711a078391ce3851f
piposaruuuu <87000315+piposaruuuu@users.noreply.github.com>
2021-07-29 14:31:20 +0900

    マイページ修正（投稿画像）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @prototypes = Prototype.includes(:user)
+    @prototypes = @user.prototypes
   end
 
 end
